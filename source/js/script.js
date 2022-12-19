5b65ff1d206474d2d6506c3678a79f8f1f0c44d4
Aqueelon <106967535+Aqueelon@users.noreply.github.com>
2022-12-19 21:28:46 +0300

    Декоративные элементы
    Добавил скрывающую шапку на скрипте

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+const navMain = document.querySelector(".main-header__wrapper");
+const navToggle = document.querySelector(".main-header__toggle");
+
+navToggle.addEventListener("click", function () {
+  console.log("dds");
+if (navMain.classList.contains("main-header--closed")) {
+  navMain.classList.remove("main-header--closed");
+  navMain.classList.add("main-header--opened");
+}
+else {
+  navMain.classList.add("main-header--closed");
+  navMain.classList.remove("main-header--opened");
+}
+});
