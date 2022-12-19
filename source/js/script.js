09b584f9b0b8db24a204a7af20038f08fc45500d
Aqueelon <106967535+Aqueelon@users.noreply.github.com>
2022-12-19 21:40:48 +0300

    Убрал вывод в консоль

--- source/js/script.js
+++ source/js/script.js
@@ -2,8 +2,7 @@ const navMain = document.querySelector(".main-header__wrapper");
 const navToggle = document.querySelector(".main-header__toggle");
 
 navToggle.addEventListener("click", function () {
-  console.log("dds");
-if (navMain.classList.contains("main-header--closed")) {
+  if (navMain.classList.contains("main-header--closed")) {
   navMain.classList.remove("main-header--closed");
   navMain.classList.add("main-header--opened");
 }
