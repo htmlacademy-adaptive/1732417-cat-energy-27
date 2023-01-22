e370f370d1be0fd310906109405cd8064b73922f
Aqueelon <106967535+Aqueelon@users.noreply.github.com>
2023-01-23 01:43:06 +0300

    Pixel Perfect

--- source/js/script.js
+++ source/js/script.js
@@ -5,9 +5,9 @@ navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("main-header--closed")) {
   navMain.classList.remove("main-header--closed");
   navMain.classList.add("main-header--opened");
-}
+  }
 else {
   navMain.classList.add("main-header--closed");
   navMain.classList.remove("main-header--opened");
-}
+  }
 });
